2fa16edb0c8a0a759ea0de9df200aa95f2d72025
biswasbittu <biswasbittu7415@yahoo.com>
2024-04-30 11:25:28 +0530

    clint side setup done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Simple CRUD</h1>
+      <form >
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        
+
+
+
+      </form>
+      
+    </>
+  )
+}
+
+export default App
