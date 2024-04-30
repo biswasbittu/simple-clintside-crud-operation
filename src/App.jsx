dda40aaaf13c035e6623e5e2fb838a687e36eabb
biswasbittu <biswasbittu7415@yahoo.com>
2024-04-30 11:32:19 +0530

    user form done

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@
 import './App.css'
 function App() {
 
+  const handleAddUser = e=>{
+    e.preventDefault();
+//  console.log("form Work succefully");
+ const form =e.target;
+ const name =form.name.value;
+ const email =form.email.value;
+ const users={name,email}
+ console.log(users);
+  }
 
   return (
     <>
-      
       <h1>Simple CRUD</h1>
-      <form >
+      <form onSubmit={handleAddUser}>
         <input type="text" name="name" id="" />
         <br />
         <input type="email" name="email" id="" />
-        
-
-
-
+        <br />
+        <input type="submit" value="Add User" />
       </form>
-      
     </>
-  )
+  );
 }
 
 export default App
